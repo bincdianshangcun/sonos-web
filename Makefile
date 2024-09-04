6eecee80af6d24f9309231d1dd838a2e7ca34226
dianshangcun <abcdef@gmail.com>
2024-09-03 21:17:59 -0700

    M  Makefile M  run-sonos-web.sh

--- Makefile
+++ Makefile
@@ -1,3 +1,8 @@
+CURRENT_DIR := $(dir $(abspath $(firstword $(MAKEFILE_LIST))))
+TARGET_FOLDER = ${HOME}/.local/bin
+
+LIST = run-sonos-web.sh
+
 REGISTRY_NAME=webcliff
 IMAGE_NAME=sonos-web
 IMAGE_VERSION=latest
@@ -11,3 +16,11 @@ docker-build:
 
 docker-push:
 	podman push $(IMAGE_TAG)
+
+
+.PHONY: link 
+link:
+	for i in $(LIST); do \
+		rm -rf $(TARGET_FOLDER)/$$i; \
+		ln -s $(CURRENT_DIR)$$i $(TARGET_FOLDER)/$$i; \
+	done
--- run-sonos-web.sh
+++ run-sonos-web.sh
@@ -3,26 +3,22 @@
 # https://stackoverflow.com/questions/30449313/how-do-i-make-a-docker-container-start-automatically-on-system-boot
 # https://github.com/containers/podman/pull/6522
 
-
 do_syntax() {
-    echo "run-sonos-web.sh <start|stop> -d"
+    echo "run-sonos-web.sh <s1|s2|stop-s1|stop-s2> -d"
     echo 'with "-d" option, container runs in foreground, otherwise background'
 }
 
-
 # ========= Check docker =======================================
 check_container() {
-    if ! command -v podman &> /dev/null
-    then
+    if ! command -v podman &>/dev/null; then
         echo "docker"
     else
         echo "podman"
-    fi    
+    fi
 }
 
-
 # ========= Start =======================================
-do_start() {
+do_start_s1() {
     local container_cmd=$1
     shift
     local container_name=$1
@@ -33,20 +29,42 @@ do_start() {
     [[ ! -z "${debug}" ]] && bg=""
 
     $container_cmd run $bg \
-    --restart unless-stopped \
-    --net=host \
-    --name "$container_name" \
-    -e WHITELIST="Connect" \
-    --replace \
-    -t webcliff/sonos-web:latest
+        --restart unless-stopped \
+        --net=host \
+        --name "$container_name" \
+        -e PORT=5050 \
+        -e SONOS_LISTENER_PORT=4000 \
+        -e WHITELIST="Connect" \
+        --replace \
+        -t webcliff/sonos-web:latest
 }
 
+do_start_s2() {
+    local container_cmd=$1
+    shift
+    local container_name=$1
+    shift
+    local debug=$1
+
+    bg="-d"
+    [[ ! -z "${debug}" ]] && bg=""
+
+    $container_cmd run $bg \
+        --restart unless-stopped \
+        --net=host \
+        --name "$container_name" \
+        -e PORT=5051 \
+        -e SONOS_LISTENER_PORT=4001 \
+        -e WHITELIST="Move,Sub,Playbar,Play:5" \
+        --replace \
+        -t webcliff/sonos-web:latest
+}
 
 # ========= Stop =======================================
 do_stop() {
     local container_cmd=$1
     local container_name=$2
-    IMAGE=`$container_cmd ps -q --filter name=$container_name`
+    IMAGE=$($container_cmd ps -q --filter name=$container_name)
     if [[ -n $IMAGE ]]; then
         echo "stopping container $IMAGE"
         $container_cmd stop $IMAGE
@@ -57,10 +75,9 @@ do_stop() {
     fi
 }
 
-
 #### Main
 
-if [[ $# -gt 2 ]]; then 
+if [[ $# -gt 2 ]]; then
     do_syntax
     exit 1
 fi
@@ -73,16 +90,23 @@ debug=$1
 container_cmd=$(check_container)
 name="sonos-web"
 
-case "$action" in 
-    start)
-        do_start "$container_cmd" "$name" $debug
+case "$action" in
+    s1)
+        do_start_s1 "$container_cmd" "${name}-s1" $debug
         ;;
-    stop)
-        do_stop "$container_cmd" "$name"
+    s2)
+        do_start_s2 "$container_cmd" "${name}-s2" $debug
+        ;;
+    stop-s1)
+        do_stop "$container_cmd" "${name}-s1"
+        ;;
+    stop-s2)
+        do_stop "$container_cmd" "${name}-s2"
         ;;
     *)
         do_syntax
         exit 1
+        ;;
 esac
 
 exit 0
